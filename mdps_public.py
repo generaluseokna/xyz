575534363c768cdc855efc6cd031a12d2604bd24
generaluseokna <140619271+generaluseokna@users.noreply.github.com>
2024-05-25 19:02:12 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -18,7 +18,7 @@ parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 # sidebar for navigation
 with st.sidebar:
     
-    selected = option_menu('Multiple Disease Prediction System',
+    selected = option_menu('',
                           
                           ['Diabetes Prediction',
                            '',
