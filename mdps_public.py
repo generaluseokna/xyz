b37110d12b51a3303026e8727ba8f9d517f171e6
generaluseokna <140619271+generaluseokna@users.noreply.github.com>
2024-05-25 18:57:57 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May  8 21:01:15 2022
 
-@author: siddhardhan
-"""
 
 import pickle
 import streamlit as st
@@ -26,8 +21,8 @@ with st.sidebar:
     selected = option_menu('Multiple Disease Prediction System',
                           
                           ['Diabetes Prediction',
-                           'Heart Disease Prediction',
-                           'Parkinsons Prediction'],
+                           '',
+                           ''],
                           icons=['activity','heart','person'],
                           default_index=0)
     
